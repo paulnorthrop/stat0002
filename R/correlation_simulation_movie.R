48499ee888e99c49c17c98d6c17f4b9c81800701
Paul Northrop <p.northrop@ucl.ac.uk>
2020-06-25 19:25:26 +0100

    Export corr_sim_movie() !

--- R/correlation_simulation_movie.R
+++ R/correlation_simulation_movie.R
@@ -4,6 +4,7 @@
 #' @examples
 #' corr_sim_movie(rho = 0.8)
 #' corr_sim_movie(n = 10)
+#' @export
 corr_sim_movie <- function(n = 30, rho = 0, panel_plot = TRUE, hscale = NA,
                            vscale = hscale, delta_n = 1, delta_rho = 0.1, ...) {
   smovie::correlation(n = 30, rho = 0, panel_plot = TRUE, hscale = NA,
